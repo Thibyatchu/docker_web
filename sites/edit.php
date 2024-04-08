43c92bcf1ec9fec3ae4fecb12a9f52bd45deb82c
Thibyatchu <thibault.nvrt@gmail.com>
2024-04-08 15:02:38 +0200

    mj

--- sites/edit.php
+++ sites/edit.php
@@ -3,28 +3,40 @@
 session_start();
 
 if($_POST){
-    if(isset($_POST['id']) && !empty($_POST['id'])
-    && isset($_POST['login']) && !empty($_POST['login'])
-    && isset($_POST['mdp']) && !empty($_POST['mdp'])){
+    if(isset($_POST['id_pers']) && !empty($_POST['id_pers'])
+    && isset($_POST['nom']) && !empty($_POST['nom'])
+    && isset($_POST['surnom']) && !empty($_POST['surnom'])
+    && isset($_POST['age']) && !empty($_POST['age'])
+    && isset($_POST['espece']) && !empty($_POST['espece'])
+    && isset($_POST['etat_actuel']) && !empty($_POST['etat_actuel'])
+    && isset($_POST['origine']) && !empty($_POST['origine'])
+    ){
         // On inclut la connexion à la base
         require_once('includes/db.php');
         $conn = connect();
 
         // On nettoie les données envoyées
-        $id = strip_tags($_POST['id']);
-        $produit = strip_tags($_POST['login']);
-        $prix = strip_tags($_POST['mdp']);
+        $id_pers = strip_tags($_POST['id_pers']);
+        $nom = strip_tags($_POST['nom']);
+        $surnom = strip_tags($_POST['surnom']);
+        $age = strip_tags($_POST['age']);
+        $espece = strip_tags($_POST['espece']);
+        $etat_actuel = strip_tags($_POST['etat_actuel']);
+        $origine = strip_tags($_POST['origine']);
 
-        $hashedPassword = password_hash($prix, PASSWORD_DEFAULT);
 
 
-        $sql = 'UPDATE `connexion` SET `id`=:id, `login`=:login, `mdp`=:mdp WHERE `id`=:id;';
+        $sql = 'UPDATE `personnage` SET `id_pers`=:id, `nom`=:nom, `surnom`=:surnom, `age`=:age, `espece`=:espece, `etat_actuel`=:etat_actuel, `origine`=:origine WHERE `id_pers`=:id;';
 
         $query = $conn->prepare($sql);
 
         $query->bindValue(':id', $id, PDO::PARAM_INT);
-        $query->bindValue(':login', $produit, PDO::PARAM_STR);
-        $query->bindValue(':mdp', $hashedPassword, PDO::PARAM_STR);
+        $query->bindValue(':nom', $nom, PDO::PARAM_STR);
+        $query->bindValue(':surnom', $surnom, PDO::PARAM_STR);
+        $query->bindValue(':age', $age, PDO::PARAM_INT);
+        $query->bindValue(':espece', $espece, PDO::PARAM_INT);
+        $query->bindValue(':etat_actuel', $etat_actuel, PDO::PARAM_INT);
+        $query->bindValue(':origine', $origine, PDO::PARAM_INT);
 
         $query->execute();
 
@@ -45,7 +57,7 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
     // On nettoie l'id envoyé
     $id = strip_tags($_GET['id']);
 
-    $sql = 'SELECT * FROM `connexion` WHERE `id` = :id;';
+    $sql = 'SELECT * FROM `personnage` WHERE `id_pers` = :id;';
 
     // On prépare la requête
     $query = $conn->prepare($sql);
@@ -94,16 +106,31 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
                 <h1>Modifier un compte</h1>
                 <form method="post">
                     <div class="form-group">
-                        <label for="login">Identifiant</label>
-                        <input type="text" id="login" name="login" class="form-control" value="<?= htmlspecialchars($produit['login'])?>">
+                        <label for="nom">Identifiant</label>
+                        <input type="text" id="nom" name="nom" class="form-control" value="<?= htmlspecialchars($produit['nom'])?>">
 
                     </div>
                     <div class="form-group">
-                        <label for="mdp">Mot de passe</label>
-                        <input type="text" id="mdp" name="mdp" class="form-control" value="<?= $produit['mdp']?>">
-
+                        <label for="surnom">Mot de passe</label>
+                        <input type="text" id="surnom" name="surnom" class="form-control" value="<?= $produit['surnom']?>">
                     </div>
-                    <input type="hidden" value="<?= $produit['id']?>" name="id">
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input type="text" id="age" name="age" class="form-control" value="<?= htmlspecialchars($produit['age'])?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="espece">Espèce</label>
+                        <input type="text" id="espece" name="espece" class="form-control" value="<?= htmlspecialchars($produit['espece'])?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="etat_actuel">Etat actuel</label>
+                        <input type="text" id="etat_actuel" name="etat_actuel" class="form-control" value="<?= htmlspecialchars($produit['etat_actuel'])?>">
+                    </div>        
+                    <div class="form-group">
+                        <label for="origine">Origine</label>
+                        <input type="text" id="origine" name="origine" class="form-control" value="<?= htmlspecialchars($produit['origine'])?>">
+                    </div>       
+                    <input type="hidden" value="<?= $produit['id_pers']?>" name="id">
                     <button class="btn btn-primary">Envoyer</button>
                 </form>
             </section>
